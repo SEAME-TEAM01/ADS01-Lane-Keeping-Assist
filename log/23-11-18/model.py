6afbae610c62fca78ca4bc1ecb2c16863210f6a6
Shuta-syd <shuta.ogura0921@gmail.com>
2023-11-21 00:55:00 +0100

    fix log dir name

--- log/23-11-18/model.py
+++ log/23-11-18/model.py
@@ -1,35 +1,38 @@
 # model.py
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Input, Rescaling, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, ReLU, Lambda, Activation
 
 def encoder(inputs, outchan):
-  x = Conv2D(outchan, (3,3), activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+  x = Conv2D(outchan, (3,3), activation='relu', padding='same')(inputs)
+
   x = Dropout(0.1)(x)
-  x = Conv2D(outchan, (3,3), activation='relu', padding='same', kernel_initializer='he_normal')(x)
+
+  x = Conv2D(outchan, (3,3), activation='relu', padding='same')(x)
 
   p = MaxPooling2D((2,2), strides=2)(x)
   return x, p
 
 def decoder(inputs, skip_features, outchan):
   x = Conv2DTranspose(outchan, (2,2), strides=2, padding='same')(inputs)
+  # Copy and crop the skip features to match the shape of the upsampled input
+  skip_features = tf.image.resize(skip_features, size=(x.shape[1],x.shape[2]))
 
   x = concatenate([x, skip_features])
-  x = Conv2D(outchan, (3,3), activation='relu', padding='same', kernel_initializer='he_normal')(x)
+  x = Conv2D(outchan, (3,3), activation='relu', padding='same')(x)
   x = Dropout(0.2)(x)
-  x = Conv2D(outchan, (3,3), activation='relu', padding='same', kernel_initializer='he_normal')(x)
+  x = Conv2D(outchan, (3,3), activation='relu', padding='same')(x)
   return x
 
 def bottleneck(inputs, outchan):
-  x = Conv2D(outchan, (3,3), activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-  x = Conv2D(outchan, (3,3), activation='relu', padding='same', kernel_initializer='he_normal')(x)
-  x = Dropout(0.5)(x)
+  x = Conv2D(outchan, (3,3), activation='relu', padding='same')(inputs)
+  x = Dropout(0.3)(x)
+  x = Conv2D(outchan, (3,3), activation='relu', padding='same')(x)
   return x
 
 def unet_model(HEIGHT, WIDTH, CHANNELS):
   input_tensor = Input((HEIGHT, WIDTH, CHANNELS))
-  s = tf.keras.layers.Lambda(lambda x: x/255)(input_tensor)
+  s = input_tensor
 
   # Contracting Path
   c1,p1 = encoder(s, 64)
@@ -50,7 +53,7 @@ def unet_model(HEIGHT, WIDTH, CHANNELS):
 
   # Expansive Path inst
   c6_ins = decoder(b1, c4, 512)
-  c7_ins = decoder(c6_ins, c3, 256)
+  c7_ins = decoder(c6_ins, c3, 258)
   c8_ins = decoder(c7_ins, c2, 128)
   c9_ins = decoder(c8_ins, c1, 64)
 
@@ -58,7 +61,6 @@ def unet_model(HEIGHT, WIDTH, CHANNELS):
   c9_ins = ReLU()(c9_ins)
   ins_seg = Conv2D(4, (1,1), activation='sigmoid', name='ins_seg')(c9_ins)
 
-  model = Model(inputs=[input_tensor], outputs=[bin_seg])
+  model = Model(inputs=[input_tensor], outputs=[bin_seg, ins_seg])
 
-  model.compile(optimizer=Adam(learning_rate=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
   return model
