bf43460782f13cd145c3a9583702f171d01cf401
Shuta-syd <shuta.ogura0921@gmail.com>
2023-11-24 18:29:57 +0100

    Refactor clustering code to use HDBSCAN algorithm

--- src/clustering.py
+++ src/clustering.py
@@ -11,34 +11,34 @@ from dotenv import load_dotenv
 from predict import test_predict
 load_dotenv('.env')
 
-Lane = LaneDataset(train=True)
-SAMPLE_IMAGES = Lane.X_train
-model = keras.models.load_model(os.getenv('MODEL_PATH'), custom_objects={'dice_coef': dice_coef, 'dice_loss': dice_loss})
+def HDBSCAN_cluster(lanes):
+  """
+  Clusters a list of lane coordinates using HDBSCAN.
+
+  params lanes: List of (x, y) tuples representing lane coordinates.
+  """
+
+  lanes_df = lanes_to_dataframe(lanes)
+
+  scaler = MinMaxScaler()
+  scaler.fit(lanes_df[['x']])
+  lanes_df['x'] = scaler.transform(lanes_df[['x']])
+  scaler.fit(lanes_df[['y']])
+  lanes_df['y'] = scaler.transform(lanes_df[['y']])
+
+  cluster = hdbscan.HDBSCAN(min_cluster_size=10)
+  cluster_labels = cluster.fit_predict(lanes_df)
+  lanes_df['cluster'] = cluster_labels
+  return lanes_df
+
 
 def lanes_to_dataframe(lanes):
     """
     Converts a list of lane coordinates to a Pandas DataFrame.
+
+    param lanes: List of (x, y) tuples representing lane coordinates.
     """
     df = pd.DataFrame(lanes, columns=['x', 'y'])
     df = df.sort_values(by='y', ascending=False)
     df = df.reset_index(drop=True)
     return df
-
-lanes = test_predict(SAMPLE_IMAGES, model)
-lanes_df = lanes_to_dataframe(lanes)
-plt.scatter(lanes_df['x'], lanes_df['y'])
-plt.show()
-
-scaler = MinMaxScaler()
-scaler.fit(lanes_df[['x']])
-lanes_df['x'] = scaler.transform(lanes_df[['x']])
-scaler.fit(lanes_df[['y']])
-lanes_df['y'] = scaler.transform(lanes_df[['y']])
-print(lanes_df)
-
-clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
-cluster_labels = clusterer.fit_predict(lanes_df)
-lanes_df['cluster'] = cluster_labels
-print(lanes_df)
-plt.scatter(lanes_df['x'], lanes_df['y'], c=lanes_df['cluster'])
-plt.show()
