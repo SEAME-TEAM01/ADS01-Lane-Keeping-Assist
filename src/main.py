e224d7aff2b085ac2fa4da7392ba528a01243cdc
seame-workspace01 <shuta.ogura@protonmail.com>
2023-12-05 14:52:48 +0100

    Refactoring main.py World HUD class add

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import pygame
 import carla
+from carla_class.HUD import HUD
+from carla_class.World import World
 
 HOST = 'localhost'
-PORT = 3000
+PORT = 2000
 
 def main():
   pygame.init()
@@ -14,8 +16,24 @@ def main():
     client.set_timeout(2.0)
 
     display = pygame.display.set_mode((800, 600), pygame.HWSURFACE | pygame.DOUBLEBUF)
+    hud = HUD(800, 600)
+    world = World(client.get_world(), hud, "vehicle.*")
+    
+
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick_busy_loop(60)
+        world.tick(clock)
+        world.render(display)
+        pygame.display.flip()    
+    
+
   finally:
-     pass
+    if world is not None:
+        world.destroy()
+
+    pygame.quit()
+
 
 if __name__ == '__main__':
     main()
