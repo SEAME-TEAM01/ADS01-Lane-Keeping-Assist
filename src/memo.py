64d44be1b488b657a5a735bc679d884d0c5f3dd4
Shuta-syd <shuta.ogura0921@gmail.com>
2023-12-12 18:51:12 +0100

    fix bug add pure_pursuit() I added before

--- src/memo.py
+++ src/memo.py
@@ -1,10 +1,12 @@
 import os
+import math
 import numpy as np
 import cv2
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow.keras as keras
-from clustering import HDBSCAN_cluster,visualize_cluster
+from clustering import HDBSCAN_cluster
+from dataset import LaneDataset
 from dotenv import load_dotenv
 from loss import dice_coef, dice_loss
 load_dotenv('.env')
@@ -12,9 +14,6 @@ load_dotenv('.env')
 def create_mask(pred_mask):
   mask = pred_mask[..., -1] >= 0.5
   pred_mask[..., -1] = tf.where(mask, 1, 0)
-
-  # only if you drive on highway this is
-  pred_mask[0, :120, :, :] = 0
   return pred_mask[0]
 
 def extract_current_lanes(df_lanes=None, width=512):
@@ -90,6 +89,7 @@ def pure_pursuit(reference_path=None, width=512, height=256):
   if lookahead_point is None:
     return None
 
+  print(lookahead_point)
   angle_to_lookahead_radian = np.arctan2(vehicle_position[1] - lookahead_point[1], lookahead_point[0] - vehicle_position[0])
   return angle_to_lookahead_radian
 
@@ -105,75 +105,10 @@ def mask_to_coordinates(mask):
   coords = list(zip(x, y))
   return coords
 
-def draw_lanes(lanes=None, left_lane=None, right_lane=None):
-  """
-  Draws the lanes on the image
-  """
-  background = np.zeros((256, 512), dtype=np.uint8)
-  if lanes is None:
-    for x, y in zip(left_lane['x'], left_lane['y']):
-      background[y, x] = 255
-    for x, y in zip(right_lane['x'], right_lane['y']):
-      background[y, x] = 255
-
-  if (left_lane is None) and (right_lane is None):
-    background = np.zeros((256, 512), dtype=np.uint8)
-    for x, y in lanes:
-      background[y, x] = 255
-  plt.imshow(background)
-  plt.show()
-  return background
-
-
-def display_heading_line(image, left_lane, right_lane, steering_angle, frame):
-    """
-    Draws heading line on the image based on left and right lane coordinates and steering angle.
-
-    Args:
-    - image: A TensorFlow EagerTensor representing the image.
-    - left_lane: Coordinates of the left lane.
-    - right_lane: Coordinates of the right lane.
-    - steering_angle: The calculated steering angle.
-
-    Returns:
-    - Image tensor with the heading line drawn.
-    """
-    image_with_line = np.copy(image)
-
-    for x, y in zip(left_lane['x'], left_lane['y']):
-      image_with_line[y, x] = 255
-    for x, y in zip(right_lane['x'], right_lane['y']):
-      image_with_line[y, x] = 255
-
-    height, width = image_with_line.shape[:2]
-    center_x, center_y = width // 2, height
-    line_length = height // 3  # Adjust the length of the heading line as needed
-    angle_radians = np.deg2rad(180 - steering_angle)  # Convert to radians
-
-    # Calculate end point of the heading line
-    end_x = int(center_x + line_length * np.cos(angle_radians))
-    end_y = int(center_y - line_length * np.sin(angle_radians))
-
-    image_with_line = cv2.line(image_with_line, (center_x, center_y), (end_x, end_y), (0, 0, 255), 3)
-    cv2.imwrite(f'_pred_out/{frame}_{steering_angle}.jpg', image_with_line)
-    # plt.imshow(image_with_line)
-    # plt.show()
-
-def display_mask(image, mask):
-  """
-  Displays the image and the mask
-  """
-  plt.figure(figsize=(15, 15))
-  title = ['Input Image', 'Predicted Mask']
-  display_list = [image, mask]
-  for i in range(len(display_list)):
-    plt.subplot(1, len(display_list), i+1)
-    plt.title(title[i])
-    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
-    plt.axis('off')
-  plt.show()
-
 def plot_heading_line(image, steering_angle, width=512, height=256):
+  """
+
+  """
   center_x, center_y = width // 2, height
   line_length = height // 3
   end_x = int(center_x + line_length * np.cos(steering_angle))
@@ -214,34 +149,25 @@ def plot_lines(image, left_lane, right_lane, path_points, plot=False):
     return image_with_line
 
 
-def predict_steering_angle(image, model=None, frame=1):
+def predict(image, model=None):
   """
   Predict Steering Angle from Live Image
   """
-  try:
-    img = tf.expand_dims(image, 0)
-    pred_mask = model.predict(img)
-    mask = create_mask(pred_mask)
-    # display_mask(image, mask)
-    lanes_coords = mask_to_coordinates(mask)
-    df_lanes = HDBSCAN_cluster(lanes_coords)
-    # visualize_cluster(df_lanes)
-    left_lane, right_lane = extract_current_lanes(df_lanes=df_lanes)
-    # draw_lanes(left_lane=left_lane, right_lane=right_lane)
-    path_points = calculate_ideal_path(left_lane=left_lane, right_lane=right_lane)
-    steering_angle = pure_pursuit(left_lane, right_lane, width=512, height=256)
-    image = plot_lines(image, left_lane, right_lane, path_points)
-    # plot_heading_line(image, steering_angle)
-  except Exception as e:
-    steering_angle = -1
+  img = tf.expand_dims(image, 0)
+  pred_mask = model.predict(img)
+  mask = create_mask(pred_mask)
+  lanes_coords = mask_to_coordinates(mask)
+  df_lanes = HDBSCAN_cluster(lanes_coords)
+  left_lane, right_lane = extract_current_lanes(df_lanes=df_lanes) # return dataframe x, y coordinates
+  path_points = calculate_ideal_path(left_lane=left_lane, right_lane=right_lane)
+  steering_angle = pure_pursuit(reference_path=path_points)
+  print(steering_angle)
+  image = plot_lines(image, left_lane, right_lane, path_points)
+  plot_heading_line(image, steering_angle)
   return steering_angle
 
-# def main():
-#   Lane = LaneDataset(train=True)
-#   SAMPLE_IMAGES = Lane.X_train
-#   model = keras.models.load_model(os.getenv('MODEL_PATH'), custom_objects={'dice_coef': dice_coef, 'dice_loss': dice_loss})
-#   for img in SAMPLE_IMAGES.take(10):
-#     predict_steering_angle(img, model)
-
-# if __name__ == '__main__':
-#   main()
+Lane = LaneDataset(train=True)
+SAMPLE_IMAGES = Lane.X_train
+model = keras.models.load_model(os.getenv('MODEL_PATH'), custom_objects={'dice_coef': dice_coef, 'dice_loss': dice_loss})
+for img in SAMPLE_IMAGES.take(10):
+  predict(img, model)
