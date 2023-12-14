69f44e696fc6fdc77afd86e08182c68ab087ca37
seame-workspace01 <shuta.ogura@protonmail.com>
2023-12-14 02:36:30 +0100

    adapt multiprocess

--- src/carla_class/World.py
+++ src/carla_class/World.py
@@ -7,10 +7,10 @@ import pygame
 import numpy as np
 import cv2
 import tensorflow as tf
-from queue import Queue
+from multiprocessing import Queue
 
 class World(object):
-    def __init__(self, carla_world, hud, actor_filter):
+    def __init__(self, carla_world, hud, actor_filter, img_queue):
         self.world = carla_world
         self.map = self.world.get_map()
         self.control = carla.VehicleControl()
@@ -34,7 +34,7 @@ class World(object):
             # carla.Transform(carla.Location(x=-29.967329, y=406.546173, z=-11.734661), carla.Rotation(pitch=0.000000, yaw=-2.000000, roll=-0.000000))
         ]
         self.world.on_tick(hud.on_world_tick)
-        self.img_queue = Queue(maxsize=20)
+        self.img_queue = img_queue
         self.restart()
         cam_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
         cam_bp.set_attribute("image_size_x",str(512))
@@ -117,14 +117,9 @@ class World(object):
 
     def process_image(self, image):
         image.save_to_disk('_out/%08d.jpeg' % image.frame)
-        img = tf.io.read_file('_out/%08d.jpeg' % image.frame)
-        os.remove('_out/%08d.jpeg' % image.frame)
-        img = tf.image.decode_jpeg(img, channels=3) 
-        img = tf.image.convert_image_dtype(img, tf.float32)
-        img = tf.image.resize(img, [256, 512], method='nearest')
         if (self.img_queue.full() == True):
             self.img_queue.get()
-        self.img_queue.put(img)
+        self.img_queue.put('_out/%08d.jpeg' % image.frame)
 
     def modify_vehicle_physics(self, actor):
         #If actor is not a vehicle, we cannot use the physics control
